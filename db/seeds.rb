cd82959ec24994ecccaf18b8dfc7b16288cfc22f
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-04-12 17:48:15 +0000

    cp27: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+ 
 # Create Topics
 15.times do
   Topic.create!(
@@ -19,6 +29,7 @@ topics = Topic.all
 # Create Posts
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -34,7 +45,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'tmoolmuang@yahoo.com', # replace this with your personal email
+  password: 'helloworld'
+)
+ 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
